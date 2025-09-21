8c47e3503811c85423aba8fc60da8c9502cbefe4
syahril20 <rodyulo20@gmail.com>
2025-09-22 03:49:31 +0700

    First Commit

---
+++ src/main/java/testing/belajar/controller/HelloController.java
@@ -0,0 +1,21 @@
+package testing.belajar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, Object> sayHello() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Hellos Watahel!");
+        response.put("status", "success");
+        response.put("code", 200);
+        return response;
+    }
+
+}
--- src/test/java/testing/belajar/BelajarApplicationTests.java
+++ src/test/java/testing/belajar/BelajarApplicationTests.java
@@ -8,6 +8,7 @@ class BelajarApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
+        // TODO document why this method is empty
+    }
 
 }
